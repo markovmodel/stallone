a431d6e36b550f463ace3aaf1a9304a56c07c80a
marscher <m.scherer@fu-berlin.de>
2013-10-11 18:45:14 +0200

    first build jcc wrapper in custom builder (create a binary egg) and install it with a custom installer

--- pystallone/setup.py
+++ pystallone/setup.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python
-import subprocess
-import shlex
-import sys
-from os import listdir, pathsep
+from os import listdir, rmdir
 from os.path import isfile, join
+import shlex
+import shutil
+import subprocess
+import sys
+from tempfile import mkdtemp
+from zipfile import ZipFile
+
 
 # prefer setuptools in favor of distutils
 try:
     from setuptools.core import setup
     from setuptools.command.build import build
+    from setuptools.command.install import install
+    from setuptools.command.clean import clean
 except ImportError:
     from distutils.core import setup
     from distutils.command.build import build
+    from distutils.command.install import install
+    from distutils.command.clean import clean
 
 stallone_api_jar = 'stallone-1.0-SNAPSHOT-api.jar'
 
 __version__ = '1.0'
 __name__ = 'pystallone'
+
 lib_dir = 'libs/'
 
 def create_class_path():
@@ -26,7 +35,11 @@ def create_class_path():
     return cp_string
 
 class mybuild(build):
+    """
+    invokes apache jcc to build a wrapper for the public api of stallone 
+    """
     def run(self):
+        build.run(self)
         classpath = create_class_path()
         includepath = classpath.replace('--classpath', '--include')
         
@@ -34,23 +47,26 @@ class mybuild(build):
              + classpath + includepath \
              + " --python " + __name__ + \
             " --version " + __version__ + " --build --reserved extern" + \
-            " --module util/ArrayWrapper --files 4"
+            " --module util/ArrayWrapper --files 4 --bdist"
         
-        return subprocess.call(shlex.split(call))
+        subprocess.call(shlex.split(call))
 
-def list_files():
-    """
-    maps files in package build directory for installation
-    """
-    package_dir = 'build/' + __name__ + '/'
-    files = [ package_dir + f for f in listdir(package_dir)]
-    return [(__name__ + '/', files)]
+class myinstall(install):
+        def run(self):
+            dest = self.install_usersite
+            zfile = ZipFile('dist/' + listdir('dist')[0])
+            tmp = mkdtemp()
+            zfile.extractall(path=tmp)
+            self.copy_tree(tmp, dest)
+            shutil.rmtree(tmp)
+            
+class myclean(clean):
+    def run(self):
+        shutil.rmtree('build')
     
 setup(name=__name__,
       version=__version__,
-      cmdclass=dict(build=mybuild),
-      package='build/pystallone',
-      data_files=list_files(),
+      cmdclass=dict(build=mybuild, install=myinstall, clean=myclean),
       # runtime dependencies
       requires=['jcc (>=1.6)'],
       )
