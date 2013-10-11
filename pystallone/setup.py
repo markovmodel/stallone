279b0c6c8695d7d4c5c6a75514a0ef35db257f9f
marscher <m.scherer@fu-berlin.de>
2013-10-11 17:31:13 +0200

    building and installation of pystallone wrapper works

--- pystallone/setup.py
+++ pystallone/setup.py
@@ -1,37 +1,56 @@
 #!/usr/bin/env python
 import subprocess
 import shlex
+import sys
+from os import listdir, pathsep
+from os.path import isfile, join
 
-# prefer setuptools in favour of distutils
+# prefer setuptools in favor of distutils
 try:
     from setuptools.core import setup
-    from setuptools.command.build import build as _build_clib
+    from setuptools.command.build import build
 except ImportError:
     from distutils.core import setup
-    from distutils.command.build import build as _build_clib
-    
+    from distutils.command.build import build
 
-stallone_api_jar = '../target/'
-classpath = []
+stallone_api_jar = 'stallone-1.0-SNAPSHOT-api.jar'
 
-class my_install(_build_clib):
+__version__ = '1.0'
+__name__ = 'pystallone'
+lib_dir = 'libs/'
+
+def create_class_path():
+    jars = [ f for f in listdir('libs/') if isfile(join('libs/', f)) ]
+    cp_string = ''.join(" --classpath " + lib_dir + "%s " \
+                        % ''.join(map(str, x)) for x in jars)
+    return cp_string
+
+class mybuild(build):
     def run(self):
-        #install.run(self)
-        _build_clib.run(self)
-        # Custom stuff here
-        # distutils.command.install actually has some nice helper methods
-        # and interfaces. I strongly suggest reading the docstrings.
+        classpath = create_class_path()
+        includepath = classpath.replace('--classpath', '--include')
         
-        call ="python -m jcc --jar " + stallone_api_jar + \
-            "--classpath lib/arpack-combo-0.1.jar --python stallone " + \
-            "--version 2.7.0 --build --reserved extern"
+        call = sys.executable + " -m jcc --jar " + lib_dir + stallone_api_jar \
+             + classpath + includepath \
+             + " --python " + __name__ + \
+            " --version " + __version__ + " --build --reserved extern" + \
+            " --module util/ArrayWrapper --files 4"
         
-        print "huso"
-        subprocess.call(shlex.split(call))
+        return subprocess.call(shlex.split(call))
+
+def list_files():
+    """
+    maps files in package build directory for installation
+    """
+    package_dir = 'build/' + __name__ + '/'
+    files = [ package_dir + f for f in listdir(package_dir)]
+    return [(__name__ + '/', files)]
     
-setup(name='stallone',
-      version='git',
-      cmdclass=dict(build = my_install),
+setup(name=__name__,
+      version=__version__,
+      cmdclass=dict(build=mybuild),
+      package='build/pystallone',
+      data_files=list_files(),
       # runtime dependencies
-      requires=['jcc (>=1.6)']
+      requires=['jcc (>=1.6)'],
       )
