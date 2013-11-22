426779d0cc17ff259155d91a89d828c52210ab84
marscher <m.scherer@fu-berlin.de>
2013-11-22 18:44:09 +0100

    added test case for kmeans (stub, with absolute path to input file)

---
+++ test/stallone/api/cluster/TestKMeans.java
@@ -0,0 +1,34 @@
+package stallone.api.cluster;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import stallone.api.API;
+import stallone.api.datasequence.IDataReader;
+import stallone.api.datasequence.IDataSequence;
+import stallone.api.doubles.IMetric;
+
+public class TestKMeans
+{
+    IClustering algo;
+    
+    @Before
+    public void setUp() throws IOException {
+        String files = "/home/marscher/md_simulation_data/trajall_500K.xtc";
+        IDataReader loader = API.dataNew.dataSequenceLoader(files);
+        IDataSequence data = loader.load();
+        
+        IMetric metric = API.clusterNew.metric(0, 0);
+        
+        this.algo = API.clusterNew.createKmeans(data, metric, 10, 100);
+    }
+    @Test
+    public void test()
+    {
+        algo.perform();
+        System.out.println(algo.getClusterCenters());
+    }
+
+}
