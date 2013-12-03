6ec5a12bcce6256f2da40ca4c7e7145c7a7a8d9d
marscher <m.scherer@fu-berlin.de>
2013-12-03 17:37:05 +0100

    [SparseRealVector] impled a few more test cases

--- test/stallone/api/doubles/SparseRealVectorTest.java
+++ test/stallone/api/doubles/SparseRealVectorTest.java
@@ -1,11 +1,13 @@
 package stallone.api.doubles;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import stallone.api.API;
-import stallone.api.doubles.IDoubleArray;
 import stallone.doubles.SparseRealVector;
 
 public class SparseRealVectorTest
@@ -29,8 +31,14 @@ public class SparseRealVectorTest
     @Test
     public void testSet()
     {
-        vec.set(0, 23);
-        Assert.assertEquals(23, vec.get(0), tolerance);
+        vec.set(5, 23);
+        vec.set(8, 42);
+        vec.set(0, 7);
+        
+        Assert.assertEquals(7, vec.get(0), tolerance);
+        Assert.assertEquals(23, vec.get(5), tolerance);
+        Assert.assertEquals(42, vec.get(8), tolerance);
+
     }
     
     @Test
@@ -76,6 +84,13 @@ public class SparseRealVectorTest
         Assert.assertEquals(getNNZ(vec), getNNZ(copy));
         // vec[0] == copy[0]
         Assert.assertEquals(vec.get(0), copy.get(0), tolerance);
+        
+        // modify copy and ensure vec is not beeing modified.
+        copy.set(0, 42);
+        Assert.assertEquals(23, vec.get(0), tolerance);
+        
+        copy.zero();
+        Assert.assertEquals(23, vec.get(0), tolerance);
     }
     
     @Test
@@ -89,13 +104,45 @@ public class SparseRealVectorTest
     
     @Test 
     public void testNonzeroIterator() {
-        vec.set(5, 23);
-        for (IDoubleIterator it = vec.nonzeroIterator(); it.hasNext(); it.advance()) {
-            int i = it.row();
-            int j = it.column();
-            System.out.println("i:  " + i + "; j : " + j);
-            double v = it.get();
-            Assert.assertEquals(23f, v, tolerance);
+        class result {
+            public result(double v, int i, int j) {
+                this.v=v; this.i=i;this.j=j;
+            }
+            double v;
+            int i, j;
+            
+            public boolean equals(Object obj) {
+                if (obj instanceof result)
+                {
+                    result r = (result) obj;
+                    return i == r.i && j == r.j && (Math.abs(v - r.v) < tolerance);
+                }
+                return false;
+            }
+            
+            public String toString() {
+                return "("+ i + "," + j + ") = " + v;
+            }
         }
+        
+        List<result> results = new ArrayList<result>();
+        List<result> expected = new ArrayList<result>();
+        
+        // note that the indices have to be sorted, since the iterator moves
+        // from beginning to end.
+        vec.set(0, 7);
+        vec.set(5, 23);
+        vec.set(8, 42);
+        
+        expected.add(new result(7f, 0, 0));
+        expected.add(new result(23f, 5, 0));
+        expected.add(new result(42f, 8, 0));
+        
+        for (IDoubleIterator it = vec.nonzeroIterator(); it.hasNext();) {
+            IDoubleElement e = it.next();
+            results.add(new result(e.get(), e.row(), e.column()));
+        }
+        
+        Assert.assertEquals(expected, results);
     }
 }
