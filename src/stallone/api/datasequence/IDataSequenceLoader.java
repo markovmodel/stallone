0f011d5f06ea40a5ffeabc5b05f4c0e740a42a5c
noe <frank.noe@fu-berlin.de>
2014-01-06 10:00:40 +0100

    Major renovation in DataSequence, Coordinates. Minor Changes in Clustering and elsewhere

---
+++ src/stallone/api/datasequence/IDataSequenceLoader.java
@@ -0,0 +1,131 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stallone.api.datasequence;
+
+import java.io.IOException;
+import java.util.List;
+import stallone.api.doubles.IDoubleArray;
+
+/**
+ *
+ * Interface for loaders that retrieve DataSequences (e.g. trajectories)
+ * from file systems or data bases. Shields the usage of this data from the
+ * file / database access in order to efficiently deal with slow loading times,
+ * while at the same time keeping the memory and file handle usage low.
+ *
+ * @author noe
+ */
+public interface IDataInput
+{
+
+    //public void setLoader(IDataReader loader);
+
+    /**
+     * Adds a link to a file or data base entry of a data sequence
+     * @param link filename or URL
+     */
+    //public void addSource(String link);
+
+    /**
+     * Scans all files or data base entries.
+     * This method needs to be called before any of the info methods (size, memorySize...) can be called.
+     * Scan will also check the file consistency, make sure that all source files have the same dimensionality
+     * and can be successfully read.
+     * It is not necessary to scan before using one of the iterables below, but in this case there is
+     * no guarantee that the iterable won't crash at a file inconsistency.
+     */
+    public void scan()
+            throws IOException;
+
+    /**
+     * Total number of sequences. Need to scan() first before calling this method.
+     * @return
+     */
+    public int numberOfSequences();
+
+    /**
+     * Dimension of the data objects.
+     * @return
+     */
+    public int dimension();
+
+    /**
+     * Total number of data objects. Need to scan() first before calling this method.
+     * @return
+     */
+    public int size();
+
+    /**
+     * size of the sequence with the given index.
+     * @param trajIndex
+     * @return
+     */
+    public int size(int trajIndex);
+
+    /**
+     * Memory requirement for the given sequence. Need to scan() first before calling this method.
+     */
+    public long memorySizeOfSingleSequence(int index);
+
+    /**
+     * Memory requirement for the largest single sequence. Need to scan() first before calling this method.
+     */
+    public long memorySizeOfLargestSequence();
+
+    /**
+     * The entire subset, as given by the index set is loaded and returned. Need to scan() first before calling this method.
+     * @param indexes nx2 array with trajectory and within-trajectory indexes
+     * @return
+     */
+    //public IDataSequences loadSubset(IIntArray indexes)
+    //        throws IOException;
+
+    /**
+     * Memory requirement for everything. Need to scan() first before calling this method.
+     */
+    public long memorySizeTotal();
+
+
+    /**
+     * Returns an iterable that can iterate over single data objects. Does not require scan() to be called.
+     * Only single data objects are loaded into memory and only one file is open at a time
+     */
+    public Iterable<IDoubleArray> getSingleDataLoader();
+
+
+    /**
+     * Returns an iterable that can iterate over single data sequences. Does not require scan() to be called.
+     * Each data sequence is fully loaded into memory at a time. Only one file is open at a time
+     * @return
+     */
+    public Iterable<IDataSequence> getSingleSequenceLoader();
+
+    /**
+     * Load a single data set. Does not require scan() to be called.
+     * @param sequenceIndex
+     * @param frameIndex
+     * @return
+     * @throws IOException
+     */
+    public IDoubleArray load(int sequenceIndex, int frameIndex)
+            throws IOException;
+
+    /**
+     * Load a single data sequence. Does not require scan() to be called.
+     * @param sequenceIndex
+     * @return
+     * @throws IOException
+     */
+    public IDataSequence loadSequence(int sequenceIndex)
+            throws IOException;
+
+    /**
+     * Loads everything into memory. Does not require scan() to be called.
+     * @return
+     */
+    public List<IDataSequence> loadAll()
+            throws IOException;
+
+}
---
+++ src/stallone/coordinates/TICA.java
@@ -0,0 +1,195 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stallone.coordinates;
+
+import static stallone.api.API.*;
+
+import stallone.api.algebra.IEigenvalueDecomposition;
+import stallone.api.coordinates.IPCA;
+import stallone.api.datasequence.IDataInput;
+import stallone.api.datasequence.IDataSequence;
+import stallone.api.doubles.IDoubleArray;
+
+import java.util.Random;
+import stallone.api.datasequence.IDataList;
+/**
+ *
+ * @author noe
+ */
+public class PCA implements IPCA
+{
+    // count and mean
+    private IDoubleArray c,  mean;
+    // product and covariance matrix
+    private IDoubleArray CC, Cov;
+    // input dimension
+    private int dimIn;
+    // total number of data points
+    private int N;
+    
+    
+    private IDoubleArray eval;
+    private IDoubleArray evec;
+    // output dimension
+    private int dimOut;
+    
+    public PCA(IDataInput _source)
+    {
+        init(_source.dimension());
+
+        for (IDataSequence seq : _source.getSingleSequenceLoader())
+            addData(seq);
+        
+        computeTransform();
+    }
+
+    public PCA(IDataSequence _source)
+    {
+        init(_source.dimension());
+        addData(_source);        
+        computeTransform();
+    }
+    
+    public PCA()
+    {
+    }
+    
+    final private void init(int _dimIn)
+    {
+        this.dimIn = _dimIn;
+        this.c = doublesNew.array(dimIn);
+        this.mean = null;
+        this.CC = doublesNew.matrix(dimIn, dimIn);
+        this.Cov = null;
+    }
+    
+
+    /**
+     * adds data to prepare the transform computation
+     * @param data The data input
+     */
+    @Override
+    final public void addData(IDataSequence data)
+    {
+        if (this.dimIn == 0 && this.N == 0)
+            init(data.dimension());
+
+        for (IDoubleArray x : data)
+        {            
+            for (int i=0; i<dimIn; i++)
+            {
+                // add counts
+                this.c.set(i, this.c.get(i)+x.get(i));
+                
+                // add product matrix
+                for (int j=i; j<dimIn; j++)
+                {
+                    double xij = x.get(i) * x.get(j);
+                    this.CC.set(i,j, this.CC.get(i,j)+xij);
+                    this.CC.set(j,i, this.CC.get(i,j));
+                }
+            }
+        }
+        
+        N += data.size();
+    }
+    
+    /**
+     * recomputes the transform based on all data added to this point. 
+     * If the coordinate transform is constant, this call has no effect.
+     * @param X A data sequence. 
+     */
+    @Override
+    final public void computeTransform()
+    {
+        mean = alg.scaleToNew(1.0/(double)N, c);
+        IDoubleArray meanT = alg.transposeToNew(mean);
+        IDoubleArray M = alg.product(mean, meanT); // matrix of <x><y>
+        Cov = alg.subtract(CC, M); // mean-free products
+        alg.scale(1.0/(double)(N-1), Cov); // covariances
+        IEigenvalueDecomposition evd = alg.evd(Cov);
+        this.eval = evd.getEvalNorm();
+        this.evec = evd.getRightEigenvectorMatrix().viewReal();
+    }
+    
+    
+    @Override
+    public void setDimension(int d)
+    {
+        dimOut = d;
+    }
+
+    @Override
+    public IDoubleArray getEigenvalues()
+    {
+        return eval;
+    }
+
+    @Override
+    public IDoubleArray getEigenvector(int i)
+    {
+        return evec.viewColumn(i);
+    }
+
+    @Override
+    public IDoubleArray getEigenvectorMatrix()
+    {
+        return evec;
+    }
+
+    @Override
+    public IDoubleArray transform(IDoubleArray x)
+    {
+        // subtract mean
+        x = alg.subtract(x, mean);
+        
+        // make a row
+        if (x.rows() > 1)
+            x = alg.transposeToNew(x);
+        
+        IDoubleArray y = alg.product(x, evec);
+        IDoubleArray yproj = doubles.subToNew(y, 0, dimOut);
+        return yproj;
+    }    
+    
+    @Override
+    public int dimension()
+    {
+        return dimOut;
+    }
+    
+    public static void main(String[] args)
+    {
+        Random rand = new Random();
+        
+        IDataList seq = dataNew.createDatalist();
+        
+        for (int i=0; i<10000; i++)
+        {
+            seq.add(doublesNew.arrayFrom(0.5*rand.nextGaussian()-2, 0.5*rand.nextGaussian()-2));
+            seq.add(doublesNew.arrayFrom(0.5*rand.nextGaussian()+2, 0.5*rand.nextGaussian()+2));
+            seq.add(doublesNew.arrayFrom(0.5*rand.nextGaussian()+4, 0.5*rand.nextGaussian()+4));
+        }
+        
+        PCA pca = new PCA();
+        pca.addData(seq);
+        pca.computeTransform();
+        
+        System.out.println("mean: \t"+doubles.toString(pca.mean, "\t"));
+        System.out.println("cov: \t"+doubles.toString(pca.Cov, "\t", "\n"));
+        System.out.println();
+        System.out.println("eval: \t"+doubles.toString(pca.getEigenvalues(), "\t"));
+        System.out.println("evec1: \t"+doubles.toString(pca.getEigenvector(0), "\t"));
+        System.out.println("evec2: \t"+doubles.toString(pca.getEigenvector(1), "\t"));
+        
+        pca.setDimension(1);
+        IDoubleArray y1 = pca.transform(doublesNew.arrayFrom(2,2));
+        System.out.println("y1 = \t"+doubles.toString(y1, "\t"));
+
+        IDoubleArray y2 = pca.transform(doublesNew.arrayFrom(4,4));
+        System.out.println("y2 = \t"+doubles.toString(y2, "\t"));
+        
+    }
+}
