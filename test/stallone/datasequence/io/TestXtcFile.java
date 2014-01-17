a12414d935211069551cbf432eddb8f2bbd9da05
marscher <m.scherer@fu-berlin.de>
2014-01-17 17:13:00 +0100

    added test stub for XtCFile

---
+++ test/stallone/datasequence/io/TestXtcFile.java
@@ -0,0 +1,136 @@
+package stallone.datasequence.io;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestXtcFile
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @Test
+    public void testXtcFile()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testXtcFileString()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testSetSource()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testScan()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testNDOF()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testNAtoms()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testNFrames()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testFrameHasPosition()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testGetFrameSizeInt()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testGetFrameSizeIntBoolean()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testReadFrame()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testGetPositionsAt()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testGetPrecisionAt()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testGetForcesAt()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testGetBoxAt()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testGetNumberOfAtomsAt()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testGetStepAt()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testGetTimeAt()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testClose()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testOpen()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
