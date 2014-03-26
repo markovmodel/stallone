eb5d4e9146ba79b06eefd4ffe15510d1e3796115
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2014-03-26 11:13:12 +0100

    [T sampling w/ dG dist] minor changes

--- src/stallone/mc/sampling/TransitionMatrixSamplerRevDeltaG.java
+++ src/stallone/mc/sampling/TransitionMatrixSamplerRevDeltaG.java
@@ -23,71 +23,73 @@ public class TransitionMatrixSamplerRevDeltaG extends TransitionMatrixSamplerAbs
 
     private IDeltaGDistribution deltaG;
 
-    public TransitionMatrixSamplerRevDeltaG(IDoubleArray counts)
+    public TransitionMatrixSamplerRevDeltaG( IDoubleArray counts, IDeltaGDistribution _deltaG )
     {
-        super(counts);
+        super( counts );
+        deltaG = _deltaG;
     }
 
-    public TransitionMatrixSamplerRevDeltaG(IDoubleArray counts, IDoubleArray Tinit)
+    public TransitionMatrixSamplerRevDeltaG( IDoubleArray counts, IDeltaGDistribution _deltaG, IDoubleArray Tinit )
     {
-        super(counts, Tinit);
+        super( counts, Tinit );
+        deltaG = _deltaG;
     }
 
     @Override
-    public final void init(IDoubleArray _C, IDoubleArray Tinit)
+    public final void init( IDoubleArray _C, IDoubleArray Tinit )
     {
         this.C = _C;
 
-        if (Tinit == null)
+        if ( Tinit == null )
         {
-            this.T = MarkovModel.util.estimateTrev(eraseNegatives(_C));
+            this.T = MarkovModel.util.estimateTrev( eraseNegatives( _C ) );
         }
         else
         {
             this.T = Tinit;
         }
-        this.logLikelihood = MarkovModel.util.logLikelihood(T, C);
+        this.logLikelihood = MarkovModel.util.logLikelihood( T, C );
 
         // stationary distribution
-        mu = msm.stationaryDistribution(T);
+        mu = msm.stationaryDistribution( T );
 
         // initialize steps
         this.step_row = new Step_Rev_Row_Beta();
-        this.step_row.init(C, T, mu);
+        this.step_row.init( C, T, mu );
         this.step_quad = new Step_Rev_Quad_MC();
-        this.step_quad.init(C, T, mu);
+        this.step_quad.init( C, T, mu );
 
         // weights
         int dofQuad=0, dofRow=0;
-        for (int i=0; i<C.rows(); i++)
+        for ( int i=0; i<C.rows(); i++ )
         {
-            if (C.get(i,i) > -1 && doubles.sumRow(C, i) > - C.columns())
+            if ( ( C.get(i,i) > -1 ) && ( doubles.sumRow( C, i ) > - C.columns() ) )
                 dofRow++;
-            for (int j=i+1; j<C.columns(); j++)
+            for ( int j=i+1; j<C.columns(); j++ )
             {
-                if (C.get(i,j)+C.get(j,i) > -2)
+                if ( C.get(i,j)+C.get(j,i) > -2 )
                     dofQuad++;
             }
         }
-        this.p_step_row = (double)dofRow / (double)(dofQuad + dofRow);
+        this.p_step_row = (double) dofRow / (double) ( dofQuad + dofRow );
     }
 
-    public static TransitionMatrixSamplerRevDeltaG create(IDoubleArray _C, IDoubleArray Tinit, IReversibleSamplingStep _step_row, IReversibleSamplingStep _step_quad)
+    public static TransitionMatrixSamplerRevDeltaG create( IDoubleArray _C, IDoubleArray Tinit, IReversibleSamplingStep _step_row, IReversibleSamplingStep _step_quad )
     {
-        TransitionMatrixSamplerRevDeltaG res = new TransitionMatrixSamplerRevDeltaG(_C, Tinit);
+        TransitionMatrixSamplerRevDeltaG res = new TransitionMatrixSamplerRevDeltaG( _C, Tinit );
 
         res.step_row = _step_row;
-        res.step_row.init(_C, res.T, res.mu);
+        res.step_row.init( _C, res.T, res.mu );
 
         res.step_quad = _step_quad;
-        res.step_quad.init(_C, res.T, res.mu);
+        res.step_quad.init( _C, res.T, res.mu );
 
         return res;
     }
 
-    public static TransitionMatrixSamplerRevDeltaG create(IDoubleArray counts, IReversibleSamplingStep _step_row, IReversibleSamplingStep _step_quad)
+    public static TransitionMatrixSamplerRevDeltaG create( IDoubleArray counts, IReversibleSamplingStep _step_row, IReversibleSamplingStep _step_quad )
     {
-        return create(counts, null, _step_row, _step_quad);
+        return create( counts, null, _step_row, _step_quad );
     }
 
     public IDoubleArray getInvariantDensity()
@@ -97,13 +99,13 @@ public class TransitionMatrixSamplerRevDeltaG extends TransitionMatrixSamplerAbs
 
     public double detailedBalanceError()
     {
-        return super.computeDetailedBalanceError(mu);
+        return super.computeDetailedBalanceError( mu );
     }
 
     @Override
     protected boolean step()
     {
-        if (Math.random() < this.p_step_row) // Reversible edge shift
+        if ( Math.random() < this.p_step_row ) // Reversible edge shift
         {
             return step_row.step();
         }
