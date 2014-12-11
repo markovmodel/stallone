60a4ac9733f71ed4afd9e5e4090120937249b02b
marscher <m.scherer@fu-berlin.de>
2014-12-11 14:45:36 +0100

    add annotate checksum for jar file

---
+++ ci/annotate_tag_with_jar_hash.rb
@@ -0,0 +1,27 @@
+require 'octokit'
+
+# use token from travis somehow
+token = ENV['token']
+  
+# authorize with github api token
+client = Octokit::Client.new(:access_token => token)
+
+# info about release and asset name
+tag = ENV['TRAVIS_TAG']
+jar_name = ENV['jar_name']
+
+# generate hash for jar
+text = "sha256: " + `cd target/; sha256sum #{jar_name}`
+
+# get latest release, assumes latest rel is first list element. Seems to be true.
+latest = Octokit.releases({:user => "markovmodel", :repo => "stallone" })[0]
+
+# extract release url of current tag
+release_url = latest.url
+
+old_body = latest.body
+
+new_body = old_body + '<br><br>' + text
+
+# update release body message
+client.update_release(release_url, {:body => new_body})
